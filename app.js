9d872d4d0feba1dd5eb4e3c9e25cb55a897ccd03
Darwin Diaz <152736334+darwin-dev-diaz@users.noreply.github.com>
2024-11-14 12:07:16 -0500

    Add express.urlencoded

--- app.js
+++ app.js
@@ -7,3 +7,6 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
+
+// so that the req.body fills with the data we are sending on the POST requests
+app.use(express.urlencoded({ extended: false }));
