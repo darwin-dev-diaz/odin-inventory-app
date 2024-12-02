f59d2782c97c0272d585d63b09f78cd8211f486c
Darwin Diaz <152736334+darwin-dev-diaz@users.noreply.github.com>
2024-12-02 12:06:26 -0500

    Display tags on exercise Screen

--- controllers/exercise.controller.js
+++ controllers/exercise.controller.js
@@ -8,12 +8,17 @@ const getExerciseByID = asyncHandler(async (req, res) => {
   const prerequisteExercise = await query.getExerciseByID(
     exercise.prerequisite
   );
+  const exerciseCategories = await query.getExerciseCategories(
+    req.params.exerciseID
+  );
+
   res.render("exercise", {
     name: capitalize(exercise.name),
     prereqName: capitalize(prerequisteExercise.name),
     embed: ytToEmbed(exercise.video_url),
     exercise: exercise,
     prereqExercise: prerequisteExercise,
+    categories: exerciseCategories,
   });
 });
 
--- db/queries.js
+++ db/queries.js
@@ -1,4 +1,6 @@
 const pool = require("./pool");
+const capitalize = require("../util/capitalize");
+
 async function getEverySkill() {
   const { rows } = await pool.query("SELECT * FROM skills ORDER BY id");
   return rows;
@@ -11,7 +13,27 @@ async function getExerciseByID(exerciseID) {
   return rows[0];
 }
 
+async function getExerciseCategories(exerciseID) {
+  let rows = (
+    await pool.query(
+      "SELECT category_id FROM skills_category WHERE skill_id = $1",
+      [exerciseID]
+    )
+  ).rows.map((catObj) => catObj.category_id);
+
+  const placeholders = rows
+    .map((_, index) => `$${index + 1}`)
+    .join(" OR id = ");
+  const query = "SELECT name FROM category WHERE id = " + placeholders;
+  const output = (await pool.query(query, rows)).rows.map((catObj) =>
+    capitalize(catObj.name)
+  );
+
+  return output;
+}
+
 module.exports = {
   getEverySkill,
   getExerciseByID,
+  getExerciseCategories,
 };
